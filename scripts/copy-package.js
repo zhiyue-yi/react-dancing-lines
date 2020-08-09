278ebbd37acb26e10cad62635889b790afd0adb1
ZhiyueYi <lane0223@hotmail.com>
2020-08-10 01:38:03 +0800

    update copy-package.js script

--- scripts/copy-package.js
+++ scripts/copy-package.js
@@ -5,3 +5,5 @@ const path = require('path');
 delete pkg.scripts;
 
 fs.writeFileSync(path.join(__dirname, '../dist/package.json'), JSON.stringify(pkg, null, 2));
+
+fs.copyFileSync(path.join(__dirname, '../README.md'), path.join(__dirname, '../dist/README.md'));
